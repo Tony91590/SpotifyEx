d9ce951e9ec928eb5315931c2da2507ced786ab6
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-04-06 00:44:02 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,37 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        flatDir {
+            dirs("libs")
+        }
+        google()
+        mavenCentral()
+        maven(url = "https://api.xposed.info")
+    }
+}
+
+plugins {
+    id("com.android.settings") version ("8.13.0")
+}
+
+android {
+    compileSdk = 36
+    minSdk = 27
+}
+
+rootProject.name = "RVX Spotify"
 include(":app")
-rootProject.name = "SpotifyEx"
