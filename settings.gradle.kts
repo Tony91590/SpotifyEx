12bdac3916420a1ba28c485e3aef3fa184c7c44f
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-04-06 01:43:39 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
-rootProject.name = "SpotifyEx"
-include(":app")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10" apply false
+}
