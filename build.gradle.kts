266bacb13997ece092c31bc59d1288c7c8b33a35
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-04-06 00:44:40 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        maven("https://jitpack.io")
-        maven("https://api.xposed.info")
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.9.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        maven("https://jitpack.io")
-        maven("https://api.xposed.info")
-        mavenCentral()
-    }
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
